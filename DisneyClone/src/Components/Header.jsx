c21b09da8ce1bc0fbe33001fd14467a0a934b27e
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-10-04 10:12:52 -0600

    Header
    Header

--- DisneyClone/src/Components/Header.jsx
+++ DisneyClone/src/Components/Header.jsx
@@ -1,8 +1,46 @@
 import logo from "./../assets/Images/logo.png"
+import {HiHome,HiMagnifyingGlass,HiStar,HiPlayCircle,HiTv} from "react-icons/hi2"
+import Headeritem from "./Headeritem"
+import {HiPlus,HiDotsVertical} from "react-icons/hi"
 export default function Header() {
+  const menu = [
+    {
+      name: "HOME",
+      icon: HiHome,
+    },
+    {
+      name: "SEARCH",
+      icon: HiMagnifyingGlass,
+    },
+    {
+      name: "WATCH LIST",
+      icon: HiPlus,
+    },
+    {
+      name: "ORIGINALS",
+      icon: HiStar,
+    },
+    {
+      name: "MOVIES",
+      icon: HiPlayCircle,
+    },
+    {
+      name: "SERIES",
+      icon: HiTv,
+    },
+  ];
   return (
-    <div>
-      <img src={logo} className="w-[80px] object-cover" alt="" />
+    <div className="flex items-center justify-between p-5">
+      <div className="flex gap-8 items-center">
+        <img src={logo} className="w-[80px] md:w-[115px] object-cover" alt="" />
+        {menu.map((item) => (
+          <Headeritem name={item.name} Icon={item.icon} />
+        ))}
+      </div>
+      <img
+        src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
+        className="w-[40px] rounded-full"
+      />
     </div>
   );
 }
---
+++ DisneyClone/src/Components/Headeritem.jsx
@@ -0,0 +1,8 @@
+export default function Headeritem({name,Icon}) {
+  return (
+    <div className="text-white flex items-center gap-3 text-[18px] font-semibold cursor-pointer hover:underline underline-offset-8">
+        <Icon/>
+        <h2>{name}</h2>
+    </div>
+  )
+}
