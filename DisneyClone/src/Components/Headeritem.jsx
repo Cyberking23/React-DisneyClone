685e1cac49064df5be21448c41032ea3a07a729f
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-10-04 10:54:51 -0600

    Responsive NavBar
    Nav Responsive

--- DisneyClone/src/Components/Headeritem.jsx
+++ DisneyClone/src/Components/Headeritem.jsx
@@ -1,8 +1,9 @@
 export default function Headeritem({name,Icon}) {
   return (
-    <div className="text-white flex items-center gap-3 text-[18px] font-semibold cursor-pointer hover:underline underline-offset-8">
+    <div className="text-white flex items-center gap-3 text-[18px] font-semibold cursor-pointer hover:underline
+     underline-offset-8 mb-2">
         <Icon/>
-        <h2>{name}</h2>
+        <h2 className="">{name}</h2>
     </div>
   )
 }
